29de684ef1e3d2e37807af050ca779ddec95a922
Seiya-Matsuda <64240689+Seiya-Matsuda@users.noreply.github.com>
2020-04-25 11:19:10 +0900

    Create menu.js

---
+++ menu.js
@@ -0,0 +1,12 @@
+$(function(){
+
+  $(function(){
+    $('#show').click(function(){
+      $('body').addClass('menu_open');
+    });
+    $('#hide').click(function(){
+      $('body').removeClass('menu_open');
+    });
+  });
+
+});
